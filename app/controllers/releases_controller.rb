53fbb116c1853de59bdfdc22df424bffbc287843
yuya0618 <yuya.aki.0618@gmail.com>
2019-09-27 21:06:26 +0900

    release.controller-fix

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -1,38 +1,30 @@
 class ReleasesController < ApplicationController
   before_action :set_release, only: [:show, :edit, :update, :destroy]
 
-  # GET /releases
-  # GET /releases.json
   def index
     @releases = Release.all
   end
 
-  # GET /releases/1
-  # GET /releases/1.json
   def show
     @like = ReleaseLike.where(user_id: current_user.id, release_id: params[:release_id])
   end
 
-  # GET /releases/new
   def new
     @release = Release.new
   end
 
-  # GET /releases/1/edit
   def edit
-    if @release.user_id != current_user&.id
+    if @release.user_id != current_user.id
       redirect_to release_path(@release)
+    end
   end
 
-  # POST /releases
-  # POST /releases.json
+
   def create
     @release = Release.create!(release_params)
     redirect_to root_path
   end
 
-  # PATCH/PUT /releases/1
-  # PATCH/PUT /releases/1.json
   def update
     respond_to do |format|
       if @release.update(release_params)
@@ -45,21 +37,16 @@ class ReleasesController < ApplicationController
     end
   end
 
-  # DELETE /releases/1
-  # DELETE /releases/1.json
   def destroy
     @release.destroy if @release.user_id == current_user.id
     redirect_to root_path
-    end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_release
       @release = Release.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def release_params
       params.require(:release).permit(:title, :detail).merge(user_id: current_user.id)
     end
