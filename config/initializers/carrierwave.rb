d9178a8a5638acca5963fef18bbacba7e06f9eea
yuya0618 <yuya.aki.0618@gmail.com>
2019-10-03 11:09:25 +0900

    s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,20 +1,20 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
+# require 'carrierwave/storage/abstract'
+# require 'carrierwave/storage/file'
+# require 'carrierwave/storage/fog'
 
-CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test?
-    config.storage :file
-  else
-    config.storage :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'craftsman-icon'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/craftsman-icon'
-    config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials.aws_access_key_id,
-    aws_secret_access_key: Rails.application.credentials.aws_secret_access_key,
-    region: 'ap-northeast-1'
-    }
-  end
-end
+# CarrierWave.configure do |config|
+#   if Rails.env.development? || Rails.env.test?
+#     config.storage :file
+#   else
+#     config.storage :fog
+#     config.fog_provider = 'fog/aws'
+#     config.fog_directory  = 'craftsman-icon'
+#     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/craftsman-icon'
+#     config.fog_credentials = {
+#     provider: 'AWS',
+#     aws_access_key_id: Rails.application.credentials.aws_access_key_id,
+#     aws_secret_access_key: Rails.application.credentials.aws_secret_access_key,
+#     region: 'ap-northeast-1'
+#     }
+#   end
+# end
